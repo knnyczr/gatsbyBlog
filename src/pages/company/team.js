030b9752db24e4729749a02d56a5bbb20d344896
KennyCruzer <kennycruz@Kenneths-MacBook-Pro.local>
2019-12-01 18:03:05 -0500

    initializing project

---
+++ src/pages/company/team.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+import Layout from "../../components/layout"
+
+
+const Team = () => (
+    <Layout>
+        <div>
+            <h1>Team!</h1>
+            <p>
+                Ad ex magna eu incididunt sit officia mollit non tempor aliquip. Laboris excepteur magna ipsum pariatur elit adipisicing ut incididunt id aliqua veniam reprehenderit do. Enim aliqua ipsum labore anim. Irure Lorem culpa ad Lorem et labore ex labore eu quis elit consequat ipsum. Labore sint veniam elit ad anim pariatur adipisicing eu ullamco sint ad commodo. Amet laboris Lorem quis duis.
+            </p>
+        </div>
+    </Layout>
+)
+
+export default Team; 
